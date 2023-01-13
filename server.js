9261100378fc4f04aeb15e1b7ab3997d368f78de
Samuel <samuel@samuelvafanamac.myfiosgateway.com>
2023-01-12 21:21:27 -0500

    created handlebars, views, login funtionality

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const session = require('express-session');
-const routes = require('./controllers');
+const cors = require("cors");
+const exphbs = require('express-handlebars')
+const homeRoutes = require('./controllers');
+
 
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
@@ -8,8 +11,11 @@ const SequelizeStore = require('connect-session-sequelize')(session.Store);
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+
+const handlebars= exphbs.create({})
+
 const sess = {
-  secret: process.env.DB_SESSION_SECRET,
+  secret: process.env.DB_SECRET_SESSION,
   cookie: {},
   resave: false,
   saveUninitialized: true,
@@ -19,12 +25,14 @@ const sess = {
 };
 
 app.use(session(sess));
-
+app.engine('handlebars',handlebars.engine)
+app.set('view engine', "handlebars")
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
-app.use(routes);
+app.use(homeRoutes);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log('Now listening on port ' + PORT));
 });
