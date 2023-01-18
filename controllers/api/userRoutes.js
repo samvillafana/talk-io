9d015e6190578682fd7953c94d6b08e2ddadd28f
Beto Lopez <363alberto@gmail.com>
2023-01-17 20:23:11 -0400

    added get/create/login/logout user routes

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,64 +1,74 @@
 const router = require('express').Router();
-const { User } = require('../../models');
+const { User } = require('../../models/');
 
-
+router.get('/', async (req, res) => {
+  try {
+    const userData = await User.findAll({
+      attributes: { exclude: ['password'] },
+      order: [['username', 'ASC']],
+    });
+    return res.status(200).json(userData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post('/', async (req, res) => {
   try {
+
     const userData = await User.create(req.body);
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-
-      res.status(200).json(userData);
-    });
+    console.log(req.body)
+    return res.status(200).json(userData);
+  
   } catch (err) {
+    console.log(req.body)
     res.status(400).json(err);
   }
 });
 
-router.post('/login', async (req, res) => {
-  console.log(req.body)
+router.post('/login', (req, res) => {
   try {
-    const userData = await User.findOne({ where: { username: req.body.username } });
-  
-    if (!userData) {
-      res
-        .status(400)
-        .send( 'Incorrect username or password, please try again' );
-      return;
-    }
-
-    const validPassword = await userData.checkPassword(req.body.password);
-
-    if (!validPassword) {
-      res
-        .status(400)
-        .json('Incorrect email or password, please try again' );
-      return;
-    }
-
-    req.session.save(() => {
-      req.session.user_id = userData.id;
-      req.session.logged_in = true;
-      
-      res.json({ user: userData, message: 'You are now logged in!' });
-    });
-
-  } catch (err) {
-    res.status(400).json(err);
+    User.findOne({
+      where: {
+        username: req.body.username,
+      }
+    })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+      if (!user.checkPassword(req.body.password)) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+      user.update({
+        isLoggedIn: true
+      });
+      res.status(200).json({ message: 'Logged in successfully' });
+    })
+  }
+  catch (err) {
+    res.status(500).json(err);
+    console.log(req.body)
   }
 });
 
 router.post('/logout', (req, res) => {
-  if (req.session.logged_in) {
-    req.session.destroy(() => {
-      res.status(204).end();
-    });
-  } else {
-    res.status(404).end();
-  }
+  User.findOne({
+    where: {
+      username: req.body.username,
+    }
+  })
+    .then(user => {
+      if (!user) {
+        return res.status(401).json({ message: 'Invalid username or password' });
+      }
+      user.update({
+        isLoggedIn: false
+      });
+      res.status(200).json({ message: 'Logged out successfully' });
+    })
+    .catch(err => res.status(500).json(err));
 });
+
 
 module.exports = router;
