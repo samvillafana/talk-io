3ab10242aaef8250d80c2de27dc62d52fa811e73
Beto Lopez <363alberto@gmail.com>
2023-01-17 20:39:03 -0400

    added path/http/socketIO/utils modules to server.js

--- server.js
+++ server.js
@@ -2,17 +2,26 @@ const express = require('express');
 const session = require('express-session');
 const cors = require("cors");
 const exphbs = require('express-handlebars')
-const homeRoutes = require('./controllers');
-
-
+const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const app = express();
 const PORT = process.env.PORT || 3001;
-
-
 const handlebars= exphbs.create({})
+const path = require("path");
+const http = require("http");
+const socketio = require("socket.io");
+const formatMessage = require("./utils/messages");
+require("dotenv").config();
+const {
+  userJoin,
+  getCurrentUser,
+  userLeave,
+  getRoomUsers,
+} = require("./utils/users");
+// const app = express();
+const server = http.createServer(app);
+const ioServer = socketio(server);
 
 const sess = {
   secret: process.env.DB_SECRET_SESSION,
@@ -23,7 +32,7 @@ const sess = {
     db: sequelize
   })
 };
-
+const botName = "TalkIO Bot";
 app.use(session(sess));
 app.engine('handlebars',handlebars.engine)
 app.set('view engine', "handlebars")
@@ -31,7 +40,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors());
 
-app.use(homeRoutes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on port ' + PORT));
