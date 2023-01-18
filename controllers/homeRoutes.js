f0212b9d4f0f0b62c76873d20fdbadb654787be1
krgurevich <kananyan@optonline.net>
2023-01-18 10:17:27 -0500

    update to home routes

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,6 +1,19 @@
 const router = require("express").Router();
+const { User } = require("../models");
+const withAuth = require("../utils/auth");
+
 router.get("/", async (req, res) => {
   // Send the rendered Handlebars.js template back as the response
   res.render("home");
 });
+
+router.get("/login", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/login");
+    return;
+  }
+
+  res.render("login");
+});
+
 module.exports = router;
