5ee15f4d5a3b93442587d3587263ab8cf55148ab
krgurevich <kananyan@optonline.net>
2023-01-18 12:48:01 -0500

    created handlebar for signup and updated homeRoutes for signup and login

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -7,13 +7,43 @@ router.get("/", async (req, res) => {
   res.render("home");
 });
 
+router.get("/lander/:id", withAuth, async (req, res) => {
+  try {
+    const postData = await User.findByPk(req.params.id, {
+      include: [
+        {
+          model: User,
+          attributes: ["username"],
+        },
+      ],
+    });
+
+    const post = userData.get({
+      plain: true,
+    });
+
+    res.render("lander", {
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 router.get("/login", (req, res) => {
   if (req.session.logged_in) {
-    res.redirect("/login");
+    res.redirect("/lander");
     return;
   }
 
   res.render("login");
+});
+
+router.get("/signup", (req, res) => {
+  if (req.session.logged_in) {
+    res.redirect("/lander");
+    return;
+  }
+  res.render("signup");
 });
 
 module.exports = router;
